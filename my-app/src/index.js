47beed4e6b790ea3bd0be0f06eba7f3786b332dd
rinn145 <rt_rio@mail.ru>
2024-11-01 23:11:59 +0600

    5

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,36 +8,40 @@ function App() {
       id: 1,
       name: "Category 1",
       products: [
-        { id: 1, name: "Bot telegram", price: "20$", description: "Описание товара" },
-        { id: 2, name: "Bot telegram", price: "20$", description: "Описание товара" },
-        { id: 3, name: "Bot telegram", price: "20$", description: "Описание товара" },
-        { id: 4, name: "Bot telegram", price: "20$", description: "Описание товара" },
-        { id: 5, name: "Bot telegram", price: "20$", description: "Описание товара" },
+        { id: 1, name: "Bot telegram", price: "20$", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus, perspiciatis incidunt quidem ducimus sint quibusdam, voluptatem voluptate voluptates, aut recusandae maxime modi alias minus! Nobis accusantium at quasi facere." },
+        { id: 2, name: "Bot telegram", price: "20$", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus, perspiciatis incidunt quidem ducimus sint quibusdam, voluptatem voluptate voluptates, aut recusandae maxime modi alias minus! Nobis accusantium at quasi facere." },
+        { id: 3, name: "Bot telegram", price: "20$", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus, perspiciatis incidunt quidem ducimus sint quibusdam, voluptatem voluptate voluptates, aut recusandae maxime modi alias minus! Nobis accusantium at quasi facere." },
+        { id: 4, name: "Bot telegram", price: "20$", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus, perspiciatis incidunt quidem ducimus sint quibusdam, voluptatem voluptate voluptates, aut recusandae maxime modi alias minus! Nobis accusantium at quasi facere." },
+        { id: 5, name: "Bot telegram", price: "20$", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus, perspiciatis incidunt quidem ducimus sint quibusdam, voluptatem voluptate voluptates, aut recusandae maxime modi alias minus! Nobis accusantium at quasi facere." },
       ]
     },
     {
       id: 2,
       name: "Category 2",
       products: [
-        { id: 6, name: "Website template", price: "30$", description: "Описание товара" },
-        { id: 7, name: "Website template", price: "30$", description: "Описание товара" },
-        { id: 8, name: "Website template", price: "30$", description: "Описание товара" },
-        { id: 9, name: "Website template", price: "30$", description: "Описание товара" },
-        { id: 10, name: "Website template", price: "30$", description: "Описание товара" },
+        { id: 6, name: "Website template", price: "30$", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus, perspiciatis incidunt quidem ducimus sint quibusdam, voluptatem voluptate voluptates, aut recusandae maxime modi alias minus! Nobis accusantium at quasi facere." },
+        { id: 7, name: "Website template", price: "30$", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus, perspiciatis incidunt quidem ducimus sint quibusdam, voluptatem voluptate voluptates, aut recusandae maxime modi alias minus! Nobis accusantium at quasi facere." },
+        { id: 8, name: "Website template", price: "30$", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus, perspiciatis incidunt quidem ducimus sint quibusdam, voluptatem voluptate voluptates, aut recusandae maxime modi alias minus! Nobis accusantium at quasi facere." },
+        { id: 9, name: "Website template", price: "30$", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus, perspiciatis incidunt quidem ducimus sint quibusdam, voluptatem voluptate voluptates, aut recusandae maxime modi alias minus! Nobis accusantium at quasi facere." },
+        { id: 10, name: "Website template", price: "30$", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus, perspiciatis incidunt quidem ducimus sint quibusdam, voluptatem voluptate voluptates, aut recusandae maxime modi alias minus! Nobis accusantium at quasi facere." },
       ]
     },
     {
       id: 3,
       name: "Category 3",
       products: [
-        { id: 11, name: "Mobile app", price: "50$", description: "Описание товара" },
-        { id: 12, name: "Mobile app", price: "50$", description: "Описание товара" },
-        { id: 13, name: "Mobile app", price: "50$", description: "Описание товара" },
-        { id: 14, name: "Mobile app", price: "50$", description: "Описание товара" },
-        { id: 15, name: "Mobile app", price: "50$", description: "Описание товара" },
+        { id: 11, name: "Mobile app", price: "50$", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus, perspiciatis incidunt quidem ducimus sint quibusdam, voluptatem voluptate voluptates, aut recusandae maxime modi alias minus! Nobis accusantium at quasi facere." },
+        { id: 12, name: "Mobile app", price: "50$", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus, perspiciatis incidunt quidem ducimus sint quibusdam, voluptatem voluptate voluptates, aut recusandae maxime modi alias minus! Nobis accusantium at quasi facere." },
+        { id: 13, name: "Mobile app", price: "50$", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus, perspiciatis incidunt quidem ducimus sint quibusdam, voluptatem voluptate voluptates, aut recusandae maxime modi alias minus! Nobis accusantium at quasi facere." },
+        { id: 14, name: "Mobile app", price: "50$", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus, perspiciatis incidunt quidem ducimus sint quibusdam, voluptatem voluptate voluptates, aut recusandae maxime modi alias minus! Nobis accusantium at quasi facere." },
+        { id: 15, name: "Mobile app", price: "50$", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus, perspiciatis incidunt quidem ducimus sint quibusdam, voluptatem voluptate voluptates, aut recusandae maxime modi alias minus! Nobis accusantium at quasi facere." },
       ]
     }
   ];
+
+
+
+
 
   return (
     <div className="App">
@@ -59,7 +63,7 @@ function App() {
 function ProductCard({ product }) {
   return (
     <div className="product-card">
-      <img src="path/to/image.jpg" alt={product.name} className="product-image" />
+      <img src="./tgCard.svg" alt={product.name} className="product-image" />
       <h3>{product.name}</h3>
       <p>{product.description}</p>
       <div className="product-price">{product.price}</div>
