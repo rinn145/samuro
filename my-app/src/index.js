5de6517355823682525aaf138d03a57dbceffb15
rinn145 <rt_rio@mail.ru>
2024-11-02 01:57:21 +0600

    first-finaly

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; 
+import './index.css';
+import './assets/fonts.css'; 
 
 function App() {
   const categories = [
@@ -38,9 +39,6 @@ function App() {
       ]
     }
   ];
-
-
-
 
 
   return (
