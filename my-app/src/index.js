5d92824ef6221f39781e1a1aca47ecbc8d918527
Ilya Kargapolov <ilya.kargapolov02@mail.ru>
2024-11-14 13:46:27 +0600

    new_test

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import './index.css';
 import './assets/fonts.css';
 import Navbar from './navbar';
 import Footer from './footer';
 import Content from './content';
 import ProductCard from './productCard';
-// import CatalogPage from './CatalogPage';
 
 function App() {
 
 
-
-
   return (
-
-    <div className="App">
-
-      <Navbar />
-      <ProductCard />
-      <Content />
-      <Footer />
-      {/* <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<CatalogPage />} />
-        <Route path="/product/:id" element={<ProductCard />} />
+        <div className="App">
+
+          <Navbar />
+          <ProductCard />
+          <Content />
+          <Footer />
+
+        </div>
       </Routes>
-      </Router> */}
-    </div>
+    </BrowserRouter>
 
   );
 }
