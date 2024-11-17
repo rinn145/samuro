18c69bc4fd086308f0d4c690d105203473983349
rinn145 <rt_rio@mail.ru>
2024-11-17 23:48:10 +0600

    123

--- my-app/src/navbar.jsx
+++ my-app/src/navbar.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './index.css';
 import './assets/fonts.css';
-import Samurai from './Frame.svg';
 
 function Navbar() {
   return (
     <div>
-      <section className="navbar">
+     <section className="navbar">
+      <button>
+        <Link to="/" className='nav-link'>
         <div className="logo">Samuro<br />Studio</div>
+        </Link>
+        </button>
         <div className="catalog-button">
           <i className="fas fa-list"></i>
           <span>Catalog</span>
